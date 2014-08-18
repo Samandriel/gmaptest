4de4106b393f2214017c19fbe7d14317584cdd9f
Housco <housco@gmail.com>
2014-08-18 16:52:42 +0700

    Change code structure

--- app/assets/javascripts/houses.js
+++ app/assets/javascripts/houses.js
@@ -4,36 +4,49 @@
 
 (function() {
 	window.onload = function() {
-		// Initialize map
-		var mapDiv = document.getElementById('map');
-		var latlng = new google.maps.LatLng(13.7500, 100.4667);
-		var options = {
-			center: latlng,
-			zoom: 6,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};
+		var map;
+		var mapDiv;
+		var latlng;
+		var marker;
+		var markerArray = [];
+		var myPosition; 
 
-		// Create map
-	    var map = new google.maps.Map(mapDiv, options);
-
-		// Looping through places array
-		for (var i = 0; i < 1; i++){
-			var marker = new google.maps.Marker({
-				position: latlng,
-				map: map,
-				title: "Drag marker to your location",
-				draggable: true,
-			});
-		};
+		initialize();
+		setMarker();
 
 		// Drag Marker and Update Position Value
 		google.maps.event.addListener(marker, 'dragend', function() {  
-	        var myPosition = marker.getPosition();   
+	        myPosition = marker.getPosition();   
 	        $("#house_latitude").val(myPosition.lat());
 	        $("#house_longitude").val(myPosition.lng());
 	        map.panTo(myPosition);
-	    }); 
+	    });
 
+		
+		// Initialize Map
+		function initialize() {
+			mapDiv = document.getElementById('map');
+			latlng = new google.maps.LatLng(13.7500, 100.4667);
+			var options = {
+				center: latlng,
+				zoom: 8,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+		    map = new google.maps.Map(mapDiv, options);			    	
+	    };
+
+	    // Add Marker to Map
+		function setMarker() {
+			for (var i = 0; i < 1; i++){
+			    marker = new google.maps.Marker({
+					position: latlng,
+					map: map,
+					title: "Drag marker to your location",
+					draggable: true,
+				});
+				markerArray.push(marker);
+			};			
+		};	     
 	};
 		
 })();
